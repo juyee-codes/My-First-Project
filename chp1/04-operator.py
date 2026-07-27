97e04c5501c46a384805a565136d7862d4315697
juyee-codes <juyeemandekar214@gmail.com>
2026-07-28 01:13:49 +0530

    Create 04-operator.py

---
+++ chp1/04-operator.py
@@ -0,0 +1,36 @@
+# Arithmatic Operators
+a = 7
+b = 4
+c = a + b
+print(c)
+
+# Assigment operators
+a = 4-2 # Assign 4-2 in a 
+print(a)
+b = 6
+# b += 3 # increament the value of b by 3 and then assign it to b
+b /3 # Decreament the value of b by 3 and then assign it to b
+print(b)
+
+# Comparision Opreators
+
+d = 5==5
+
+print(d)
+
+# Logical Opreators
+e = True or False 
+
+# Truth table of 'or'
+print("True or False is", True and False)
+print("True and True is", True or True)
+print("False or True is",False and True)
+print("False and True is", False and False)
+
+# Truth table of 'and'
+print("True and False is", True and False)
+print("True and True is",True and True)
+print("False and True is",False and True)
+print("False and False is", False and False)
+
+print(not (False))
